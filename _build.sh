eafbbc73e154f88189126cd57b79e04c76b2c1a8
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2019-12-17 18:49:21 +0100

    Make bookdown page (#1)
    Proof of concept using some files from https://github.com/SKDE-Analyse/dokumentasjon

---
+++ _build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export PATH=~/pandoc/1.19/usr/bin/:$PATH
+
+Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook'), encoding = 'UTF-8')"
+
+#Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
